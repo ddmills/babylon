1d09249593ce99a740f080ee2139bbdeeabb911d
ddmills <dmills@iastate.edu>
2016-05-04 21:45:37 -0500

    use arc camera

--- source/js/main.js
+++ source/js/main.js
@@ -5,7 +5,13 @@ let engine = new baby.Engine(canvas, true);
 
 BABYLON.SceneLoader.Load("", "resources/scenes/actor.babylon", engine, function (newScene) {
   newScene.executeWhenReady(function () {
-    newScene.activeCamera.attachControl(canvas);
+    let camera = new baby.ArcRotateCamera("Camera", -2.5, 1.0, 5, new baby.Vector3(0, .5, 0), newScene);
+    camera.lowerRadiusLimit = 1.35;
+    camera.upperRadiusLimit = 6;
+    camera.wheelPrecision = 100;
+    newScene.activeCamera = camera;
+
+    camera.attachControl(canvas);
     engine.runRenderLoop(function() {
       newScene.render();
     });
